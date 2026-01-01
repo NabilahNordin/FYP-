7d481d20af1e8920396ed9b51a76df16aa7db6f2
Luqman B Ahmad Nordin <luqman.nordin@LPPSA.GOV>
2026-01-01 20:58:21 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->spa()
             ->login(Login::class)
+            ->profile()
             ->sidebarCollapsibleOnDesktop()
             ->brandLogo(asset('assets/logo2.png'))
             ->brandLogoHeight('3rem')
@@ -53,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
-                Dashboard::class,
+                // Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
